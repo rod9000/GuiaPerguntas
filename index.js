285e5c63f8c2d4c27a9aa0e90e7bcddce78af33f
Rodrigo <71358562+rod9000@users.noreply.github.com>
2023-04-29 21:31:57 -0300

    ForEach
    Usando ForEach no Html

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ app.get("/:nome/:lang", (req, res) =>{
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = false; 
+
+    var produtos = [
+        {nome: "Doritos",preco: 3.14},
+        {nome: "Coca-cola",preco: 5},
+        {nome: "Leite",preco: 1.45},
+        {nome: "Carne",preco: 15},
+        {nome: "RedBull",preco: 6.45},
+        {nome: "Nescau",preco: 4.5},
+    ]
+
     res.render("index",{
         nome: nome,
         lang: lang,
         msg: exibirMsg,
+        produtos: produtos,
     });
 });
 
