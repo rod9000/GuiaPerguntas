747bd41f05bda496d59ee5bb825ea6ca90e0a79e
Rodrigo <71358562+rod9000@users.noreply.github.com>
2023-04-29 21:15:28 -0300

    Condições
    colocando condições no html

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ app.set('view engine', 'ejs');
 app.get("/:nome/:lang", (req, res) =>{
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = false; 
     res.render("index",{
         nome: nome,
         lang: lang,
+        msg: exibirMsg,
     });
 });
 
