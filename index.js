1426b3e10003605d07053843c4ebc9ecf492022f
Rodrigo <71358562+rod9000@users.noreply.github.com>
2023-04-29 21:07:42 -0300

    Atualização
    Recebendo Nome e linguagem pelo html

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 //Usando o express EJS
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) =>{
-    var nome = "Rodrigo";
-    var lang = "Java Script";
+app.get("/:nome/:lang", (req, res) =>{
+    var nome = req.params.nome;
+    var lang = req.params.lang;
     res.render("index",{
         nome: nome,
         lang: lang,
