dbf114af795d9a8ae9265772ca6e60d1748abfad
Rodrigo <71358562+rod9000@users.noreply.github.com>
2023-04-30 22:37:46 -0300

    Pagina de Perguntas
    adicionado BootStrap e pagina de perguntas e modificação do css

--- index.js
+++ index.js
@@ -4,26 +4,12 @@ const app = express();
 //Usando o express EJS
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.get("/:nome/:lang", (req, res) =>{
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false; 
+app.get("/", (req, res) =>{
+    res.render("index",{});
+});
 
-    var produtos = [
-        {nome: "Doritos",preco: 3.14},
-        {nome: "Coca-cola",preco: 5},
-        {nome: "Leite",preco: 1.45},
-        {nome: "Carne",preco: 15},
-        {nome: "RedBull",preco: 6.45},
-        {nome: "Nescau",preco: 4.5},
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        msg: exibirMsg,
-        produtos: produtos,
-    });
+app.get("/perguntar",(req, res)=>{
+    res.render("perguntar",{});
 });
 
 app.listen(8080, ()=>{ 
