8feeebb5af67ac1439abd581cab399b95691afbd
Rodrigo <71358562+rod9000@users.noreply.github.com>
2023-05-01 18:27:41 -0300

    Modal de Perguntas
    criando modal da tabela de perguntas
    e adicionado inserção na tela de perguntas e busca de dados na tabela perguntas e enviado para pagina index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser= require("body-parser");
 const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
 //DataBase
 connection.
 authenticate()
@@ -19,7 +20,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) =>{
-    res.render("index",{});
+    Pergunta.findAll({ raw: true}).then(perguntas => {
+        res.render("index",{
+            perguntas: perguntas
+        });
+    });
 });
 
 app.get("/perguntar",(req, res)=>{
@@ -29,8 +34,12 @@ app.get("/perguntar",(req, res)=>{
 app.post("/salvarpergunta",(req, res)=>{
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-
-    res.send("Formulário Recebido! Titulo: " + titulo + " Descrição: " + descricao );
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
 });
 
 
